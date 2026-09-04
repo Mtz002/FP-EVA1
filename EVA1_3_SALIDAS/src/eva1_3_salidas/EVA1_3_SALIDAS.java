118f2fc30247563c3213f44f7c0c2ca621171d6e
Mtz002 <L26550272@chihuahua2.TECNM.MX>
2026-09-03 18:43:26 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+public class EVA1_3_SALIDAS {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamentos de Programacion");
+        System.out.println("Martinez Martinez Nohemi");
+        System.out.print("Tu Calificacion será:");
+        System.out.println(85);
+
+    }
+    
+}
