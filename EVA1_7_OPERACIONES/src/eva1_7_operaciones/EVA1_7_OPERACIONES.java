fa7fcb90b8e0d184c4e1bbb0caebd08d9799033f
Mtz002 <L26550272@chihuahua2.TECNM.MX>
2026-09-14 18:54:50 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,22 @@
+
+package eva1_7_operaciones;
+
+public class EVA1_7_OPERACIONES {
+
+    public static void main(String[] args) {
+
+        int suma,vall,vall2;
+        vall= 100;
+        vall2= 50;
+        suma = vall + vall2;//expresion, asignar una vareable
+        System.out.print ("vall=");
+        System.out.print (vall);
+        System.out.print ("vall2=");
+        System.out.print (vall2);
+        System.out.print ("suma=");
+        System.out.print (suma);
+        
+        
+    }
+    
+}
