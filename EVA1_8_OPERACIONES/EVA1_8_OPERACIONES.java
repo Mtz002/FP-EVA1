bfb34cddaa04082d1714fb494fffb8f0496380e7
Mtz002 <L26550272@chihuahua2.TECNM.MX>
2026-09-14 18:55:41 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES/EVA1_8_OPERACIONES.java
@@ -0,0 +1,17 @@
+
+package eva1_8_operaciones;
+
+public class EVA1_8_OPERACIONES {
+
+    public static void main(String[] args) {
+     int resu,vall=10,vall2=5;
+     resu=100+vall*(vall/vall2);
+         System.out.print ("Resu =");
+        System.out.print (resu);
+        
+        
+        
+        
+    }
+    
+}
---
+++ EVA1_9_OPERACIONES_CADENAS/EVA1_9_OPERACIONES_CADENAS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIONES_CADENAS {
+
+    public static void main(String[] args) {
+    //Tambien hay operadores para cadenas
+    String code;
+    code= "Hola"+" "+"mundo";
+    System.out.print(code);
+    code = "Valor = " + 100;
+    System.out.append(code);
+    code = "Valores booleanos = "+false;
+    System.out.print(code);
+        
+        
+    }
+    
+}
